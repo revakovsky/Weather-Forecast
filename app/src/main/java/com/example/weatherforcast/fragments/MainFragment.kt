cf8c3e5161a1ec99e852208ba4c858fa5c5303ff
@revakovsky <revakovsky@gmail.com>
2022-07-21 15:54:38 +0300

    add Volly library

--- app/src/main/java/com/example/weatherforcast/fragments/MainFragment.kt
+++ app/src/main/java/com/example/weatherforcast/fragments/MainFragment.kt
@@ -2,6 +2,7 @@ package com.example.weatherforcast.fragments
 
 import android.Manifest
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,10 +12,15 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.weatherforcast.R
 import com.example.weatherforcast.adapters.MyViewPagerAdapter
 import com.example.weatherforcast.databinding.FragmentMainBinding
 import com.google.android.material.tabs.TabLayoutMediator
+
+const val API_KEY = "864b68ca42224665a99104528220807"
 
 class MainFragment : Fragment() {
     private lateinit var binding: FragmentMainBinding
@@ -41,6 +47,7 @@ class MainFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         checkPermission()
         initTabs()
+        requestWeatherData("London")
     }
 
     fun initTabs() = with(binding) {
@@ -51,6 +58,31 @@ class MainFragment : Fragment() {
             tab.setIcon(tabIcons[position])
 
         }.attach()
+    }
+
+    private fun requestWeatherData(city: String) {
+        val url = "https://api.weatherapi.com/v1/forecast.json?key=" +
+                API_KEY +
+                "&q=" +
+                city +
+                "&days=" +
+                "5" +
+                "&aqi=no&alerts=no"
+
+        val queue = Volley.newRequestQueue(context)
+        val request = StringRequest(
+            Request.Method.GET,
+            url,
+            {
+                result ->
+                Log.d("testLogs", "result: $result")
+            },
+            {
+                error ->
+                Log.d("testLogs", "error: $error")
+            }
+        )
+        queue.add(request)
     }
 
     private fun permissionListener() {
